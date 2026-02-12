0afc1aa453db2cc3edb73fa38c796ecc0036d63e
aaryan <aaryanthorat31@gmail.com>
2026-02-12 13:16:38 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,8 @@
 {
   "name": "server",
   "version": "1.0.0",
-  "description": "AI SaaS Backend",
   "main": "src/index.js",
   "scripts": {
     "start": "node src/index.js"
-  },
-  "author": "Aaryan",
-  "license": "ISC"
+  }
 }
